1e98e74b250d484c7794a291956fa782bce5464f
hash-smy <shammanyee@gmail.com>
2023-12-05 14:16:05 +0000

    CSS background updated

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from app.weather import WeatherAPI
 
-app = Flask(__name__)
+app = Flask(__name__) 
 weather_api = WeatherAPI()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/weather', methods=['GET'])
 def get_weather():
@@ -15,6 +19,10 @@ def get_weather():
         return jsonify({"error": "Failed to fetch weather data"}), 500
     
     return jsonify(weather_data)
+
+@app.route('/static/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
 
 if __name__ == '__main__':
     app.run(debug=True)
